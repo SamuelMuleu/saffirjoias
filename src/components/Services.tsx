ed86912fe4d7ca77703fee47080b51a0f795ddc3
Samuel Muleu <samuelpereiramuleu@gmail.com>
2024-05-24 21:08:58 -0300

    commit update

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -5,7 +5,7 @@ import aliancasemsolda1 from "../assets/aliancasemsolda.png";
 import aliancasemsolda2 from "../assets/aliancasemsolda2.png";
 import aliancasemsolda3 from "../assets/aliancasemsolda3.png";
 import aliancasemsolda4 from "../assets/aliancasemsolda6.png";
-import { v4 as uuidv4 } from 'uuid';
+
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -20,13 +20,7 @@ interface Props {
 }
 
 export default function Services({ onClick }: Props) {
-    const imagens = [
-        { id: uuidv4(), img: aliancasemsolda5 },
-        { id: uuidv4(), img: aliancasemsolda4 },
-        { id: uuidv4(), img: aliancasemsolda3 },
-        { id: uuidv4(), img: aliancasemsolda2 },
-        { id: uuidv4(), img: aliancasemsolda1 },
-    ];
+
 
     return (
 
