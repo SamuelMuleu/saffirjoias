363f6752b04a443b853303761f901220a259039b
Samuel Muleu <samuelpereiramuleu@gmail.com>
2024-05-28 23:43:00 -0300

    device breakpoits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,22 +8,8 @@ export default function Footer() {
     return (
         <div className={styles.footer}>
 
-            <div className={styles.copyright}>
-                <div>   Desenvolvido Por Samuel Muleu </div>
-                <div >Copyright © 2024, todos os direitos reservados.</div>
-            </div>
-
-
-
-
-
-            <div className={styles.dev}>  Ouro Arte Atelier de Joias Ltda
-                CNPJ: 30.284.952/0001-45</div>
-            <div  className={styles.dev}>   Avenida Pelinca, 100, Loja 66 - Parque Tamandare, Campos dos Goytacazes - RJ, 28.035-053</div>
-
-
             <div className={styles.socialmedia}>
-                acompanhe nossas novidades e lançamentos
+                Acompanhe Nossas Redes Sociais
                 <a href="https://www.facebook.com/profile.php?id=100083311750657" target="_blank">
 
                     <img src={faceSvg} alt="" className={styles.face} />
@@ -34,6 +20,20 @@ export default function Footer() {
                 </a>
 
             </div>
+
+
+
+
+
+            <div className={styles.dev}>  Ouro Arte Atelier de Joias Ltda
+                CNPJ: 30.284.952/0001-45</div>
+            <div  className={styles.dev}>   Avenida Pelinca, 100, Loja 66 - Parque Tamandare, Campos dos Goytacazes - RJ, 28.035-053</div>
+
+            <div className={styles.copyright}>
+                <div>   Desenvolvido Por Samuel Muleu </div>
+                <div >Copyright © 2024, todos os direitos reservados.</div>
+            </div>
+
         </div>
     )
 
