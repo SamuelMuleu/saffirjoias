efea9883e9cd09cf37b81427dfe0b8fe8e487d5f
Samuel Muleu <samuelpereiramuleu@gmail.com>
2024-06-11 22:21:56 -0300

    details css

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,7 @@ import Modal from 'react-modal';
 import Services from "./components/Services.tsx";
 import Header from "./components/Header.tsx";
 
-
-
-
+import CookieConsent from "react-cookie-consent";
 
 
 (window as any).global = window;
@@ -28,16 +26,26 @@ function App() {
 
 
   return (
+
+
     <BrowserRouter>
 
-
+      <CookieConsent location="bottom"
+        buttonText="Aceitar Todos Os Cookies"
+        cookieName="myAwesomeCookieName2"
+        style={{ background: '#0A1B2D', width: "25rem", height: "17.5rem", borderRadius: "8px", marginLeft: "1rem", paddingTop: '.7rem',marginBottom:'1rem' }}
+        buttonStyle={{ background: ' #E6D35B',fontWeight: 'bold'}}
+        enableDeclineButton
+        declineButtonText="Rejeitar Todos"
+        declineButtonStyle={{ background: ' #E6D35B',fontWeight: 'bold', color: "#050505" }}
+      >Ao clicar em "Aceitar todos os cookies", concorda com o armazenamento de cookies no seu dispositivo para melhorar a navegação no site, analisar a utilização do site e ajudar nas nossas iniciativas de marketing. <a href="/faleconosco" style={{display:'flex',marginRight:"7rem",marginTop:'1rem',color:' #E6D35B',opacity:'0.7',cursor:'pointer'}}>Veja Nossas Definiçôes de Cookies</a>
+      </CookieConsent>
       <Header />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/faleconosco" element={<ContactUs />} />
         <Route path="/servicos" element={<Services />} />
-
 
 
 
